02ec06a00e538d29499c8096a717fa4e2372e815
RaghavDutta <raghav.dutta29@gmail.com>
2023-11-07 23:23:15 -0500

    Updated CardView, MeetinView, Models and Theme

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Raghav Dutta on 2023-11-05.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total:15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle()
+                .stroke(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Preview:PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Raghav Dutta on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
