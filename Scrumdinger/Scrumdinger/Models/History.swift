df0abe8860da5b122455e862f86b87bb98031f0e
RaghavDutta <raghav.dutta29@gmail.com>
2023-11-12 17:36:48 -0500

    Updating app data

---
+++ Scrumdinger/Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Raghav Dutta on 2023-11-12.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
